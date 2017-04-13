4b73c27d5006a24152d4b858a56cd927d41fe5f0
seffeng <seffeng@sina.cn>
2017-04-13 16:12:00 +0800

    增加数据表字段是否索引判断方法

--- app/components/ActiveRecord.php
+++ app/components/ActiveRecord.php
@@ -5,6 +5,69 @@
 
 namespace zxf\components;
 
+use Yii;
+
 class ActiveRecord extends \yii\db\ActiveRecord {
-    
+
+    /**
+     * 返回数据库链接
+     * @date   2017年4月13日
+     * @param  string $id 数据库配置ID DEFAULT['db']
+     * [
+     *     'components'  => [
+     *         'db'      => [
+     *             'class'       => 'yii\db\Connection',
+     *             'dsn'         => 'mysql:host=localhost;dbname=dbname1',
+     *             'username'    => 'user',
+     *             'password'    => 'pass',
+     *             'charset'     => 'utf8',
+     *             'tablePrefix' => '',
+     *         ],
+     *         'db2'      => [
+     *             'class'       => 'yii\db\Connection',
+     *             'dsn'         => 'mysql:host=localhost;dbname=dbname2',
+     *             'username'    => 'user',
+     *             'password'    => 'pass',
+     *             'charset'     => 'utf8',
+     *             'tablePrefix' => '',
+     *         ],
+     *     ]
+     * ];
+     * @return object|NULL
+     */
+    public static function getDb($id=NULL) {
+        return Yii::$app->get($id ? $id : 'db');
+    }
+
+    /**
+     * 返回表内索引
+     * @date   2017年4月13日
+     * @param  string $table  表名
+     * @param  string $schema 数据库名
+     * @param  string $id     数据库链接配置
+     */
+    public static function getIndexes($table, $schema=NULL, $id=NULL) {
+        $indexes = self::getDb($id)->createCommand('SHOW INDEXES FROM '. ($schema ? ($schema .'.') : '') . $table)->queryAll();
+        $coulumn = [];
+        if ($indexes) {
+            foreach ($indexes as $val) {
+                $coulumn[] = $val['Column_name'];
+            }
+        }
+        return $coulumn;
+    }
+
+    /**
+     * 字段是否索引
+     * @date   2017年4月13日
+     * @param  string $column 字段
+     * @param  string $table  表名
+     * @param  string $schema 数据库名
+     * @param  string $id     数据库链接配置
+     * @return boolean
+     */
+    public static function isIndex($column, $table, $schema=NULL, $id=NULL) {
+        $indexes = self::getIndexes($table, $schema, $id);
+        return in_array($column, $indexes);
+    }
 }
